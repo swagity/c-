674f5907f3c623c252902e404e9e5a75ad92b5f4
Kamil Stec <ilbooks2@gmail.com>
2019-05-15 01:17:59 +0200

    keepin it simple

--- program1/Program.cs
+++ program1/Program.cs
@@ -14,31 +14,9 @@ namespace program1
         {
             for (; ; )
             {
-
-                Console.Write("WPISZ SWOJA KSYWE MORDO:");
-                string name = Console.ReadLine();
-                Console.WriteLine("elo " + name);
-                Console.Write("wpisz ile masz lat: ");
-
-                int age;
-                bool result = int.TryParse(Console.ReadLine(), out age);
-
-                if (age >= 18)
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("brawo mordziaty masz 18 lat");
-                }
-                else if (result == false)
-                {
-                    Console.WriteLine("mordziaty cos pojebałes z tym wiekiem pzdr");
-                }
-                else
-                {
-                    Console.WriteLine("mozesz pic alko ale po kryjomu");
-                }
-                Console.ReadKey();
-                Console.Clear();
-                Console.ResetColor();
+                Greeting();
+                Agerestrictions();
+                Consolesettings();
             }
 
 
@@ -46,14 +24,43 @@ namespace program1
 
 
 
-          /*  foreach (var item in args)
-            {
-                Console.WriteLine("elo,  " + item);
-                */
-            
+           
+
 
         }
 
+        private static void Consolesettings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
 
+        private static void Agerestrictions()
+        {
+            Console.Write("wpisz ile masz lat: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("brawo mordziaty masz 18 lat");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("mordziaty cos pojebałes z tym wiekiem pzdr");
+            }
+            else
+            {
+                Console.WriteLine("mozesz pic alko ale po kryjomu");
+            }
+        }
+
+        private static void Greeting()
+        {
+            Console.Write("WPISZ SWOJA KSYWE MORDO:");
+            string name = Console.ReadLine();
+            Console.WriteLine("elo " + name);
+        }
     }
 }
