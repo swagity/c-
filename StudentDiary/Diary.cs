238947ab365b28dc2d53f51525e9b81a659d903e
Kamil Stec <ilbooks2@gmail.com>
2019-05-15 15:33:10 +0200

    ss

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        List<float> grades;
+
+
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+
+        }
+        public float Average()
+        {
+            float suma = 0, avg = 0;
+            foreach (var grade in grades)
+            {
+                suma += grade;
+
+            }
+            avg = suma / grades.Count;
+            return avg;
+        }
+        public float MaxGrade()
+        {
+            return grades.Max();
+        }
+        public float MinGrade()
+        {
+            return grades.Min();
+
+        }
+
+    }
+}
